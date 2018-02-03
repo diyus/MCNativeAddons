da7b4908303843e2ede1dd7bec08c8c94abed6e7
diyus <4399d@DESKTOP-43BR2N1>
2018-02-03 20:22:37 +0800

    .

--- MCADDON/jni/main.cpp
+++ MCADDON/jni/main.cpp
@@ -8,7 +8,10 @@ class Entity{
 	public:
 	void remove();
 };
-class Player:public Entity{};
+class Player:public Entity{
+	Public:
+	void interact(Entity&);
+};
 
 class GameMode{
 	public:
@@ -16,17 +19,17 @@ class GameMode{
 	void interact(Player&,Entity&);
 };
 
-void (*attack_)(void*,Player&,Entity&);
-void (*interact_)(void*,Player&,Entity&);
+void (*attack_)(void*,Player*,Entity&);
+void (*interact_)(Player&,Entity&);
 
 void attack(void*thiz,Player&p,Entity&e){
 	e.remove();
 }
-void interact(void*thiz,Player&p,Entity&e){
-	interact_(thiz,p,e);
+void interact(Player*p,Entity&e){
+	interact_(p,e);
 }
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 	MSHookFunction((void*)&GameMode::attack, (void*) &attack, (void**) &attack_);
-	MSHookFunction((void*)&GameMode::interact, (void*) &interact, (void**) &interact_);
+	MSHookFunction((void*)&Player::interact, (void*) &interact, (void**) &interact_);
 	return JNI_VERSION_1_2;
 }
